c82b82ee772215a734ef353b1a734aa340c55b28
Gregory Dunlap <gregory.dunlap@fedex.com>
2021-06-18 16:37:10 -0500

    test code run

--- svr.py
+++ svr.py
@@ -1,3 +1,31 @@
 #!/usr/bin/python3
 
+import requests
+import json
 
+from netaddr import IPAddress, IPNetwork
+from netaddr import AddrFormatError
+from flask import Flask, request, abort
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+
+#remove the InsecureRequestWarning messages
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+"""
+api info
+"""
+
+app = Flask(__name__)
+FlaskJSON(app)
+
+@app.route('/hello')
+def hello():
+    debug = 1
+
+    say_hello_json = {'message' : 'hello'}
+    return(say_hello_json)
+#end of hello
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
